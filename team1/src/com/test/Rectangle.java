f77be43939558061597b5e6549f942db09778fb1
Purnima Gupta <eguppur@incdcs153.(none)>
2011-10-18 10:18:43 +0530

    Team 1

---
+++ team1/src/com/test/Rectangle.java
@@ -0,0 +1,51 @@
+package com.test;
+
+public class Rectangle {
+	
+	private double length;
+	
+	private double breadth;
+	
+	private double area;
+
+	/**
+	 * @return the length
+	 */
+	public double getLength() {
+		return length;
+	}
+
+	/**
+	 * @param length the length to set
+	 */
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	/**
+	 * @return the breadth
+	 */
+	public double getBreadth() {
+		return breadth;
+	}
+
+	/**
+	 * @param breadth the breadth to set
+	 */
+	public void setBreadth(double breadth) {
+		this.breadth = breadth;
+	}
+
+	/**
+	 * @return the area
+	 */
+	public double getArea() {
+		return area;
+	}
+	
+	public void calcuateArea()
+	{
+		area = length*breadth;
+	}
+
+}
