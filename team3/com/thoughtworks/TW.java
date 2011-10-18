3fec6e35de1f8ec058326e7182fb3c481ef8b63e
evaikha <vaibhav.khandelwal@ericsson.com>
2011-10-18 11:31:26 +0530

    Bitmap Updation by Team 6 :-)

--- team3/com/thoughtworks/TW.java
+++ team3/com/thoughtworks/TW.java
@@ -1,18 +1,26 @@
-package com.thoughtworks;
+package com.thoughworks;
 
 
 public class TW {
 	public static void main(String[] args) {
 		Rectangle rect = new Rectangle();
 		
-			
+		BitMap bitMap = new BitMap();
+		
+		
 		rect.length=4;
 		rect.breadth=4;
 		rect.area=rect.length*rect.breadth;
-		rect.display();
 		
-		new Circle(10).display();
+		rect.draw(bitMap);
+		bitMap.display();
 		
+		
+		new Circle(10).draw(bitMap);
+		bitMap.display();
+		
+		new Square(6).draw(bitMap);
+		bitMap.display();
 				
 		
 	}
@@ -26,18 +34,87 @@ class Circle {
 		this.radius = radius;
 	}
 	
-	public void display() {
+	/*public void display() {
 		System.out.println("radius is : "  + radius);
 		System.out.println("aread is : " + Math.PI*radius*radius);
+	}*/
+	
+	public void draw(BitMap bitMap)
+	{
+		bitMap.setDrawShape("Circle : radius = " + radius);
+		bitMap.setDrawArea("Circle : Area = " + Math.PI*radius*radius);
 	}
 }
+
 class Rectangle {
 	
 	int length;
 	int breadth;
 	int area;
-	public void display(){
+	/*public void display(){
+	
 		System.out.println("Length is: " +length + " Breadth is: "+ breadth + " Area is: " +area);
+	}*/
+	
+	public void draw(BitMap bitMap)
+	{
+		bitMap.setDrawShape("Rectangle : l = " + length + ", b = " + breadth);
+		bitMap.setDrawArea("Rectangle : Area = " + (length * breadth));
+	}
+}
+
+class Square {
+	
+	int length;
+	
+	
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public Square(int length)
+	{
+		this.length = length;
 	}
 	
+	int area;
+	/*public void display(){
+	
+		System.out.println("Length is: " +length + " Breadth is: "+ breadth + " Area is: " +area);
+	}*/
+	
+	public void draw(BitMap bitMap)
+	{
+		bitMap.setDrawShape("Square : side = " + length);
+		bitMap.setDrawArea("Square : Area = " + (length * length));
+	}
+}
+
+class BitMap
+{
+	private String drawArea;
+	private String drawShape;
+	
+	public String getDrawArea() {
+		return drawArea;
+	}
+	public void setDrawArea(String drawArea) {
+		this.drawArea = drawArea;
+	}
+	public String getDrawShape() {
+		return drawShape;
+	}
+	public void setDrawShape(String drawShape) {
+		this.drawShape = drawShape;
+	}
+	
+	public void display()
+	{
+		System.out.println(drawArea);
+		System.out.println(drawShape);
+	}
 }
